0523bbf33df837fb88dcd478900b31940e1bafd2
Md-Siam-Mia-Code <mdsiammia.main@gmail.com>
2024-11-01 15:32:49 +0600

    Update OpenWebUI.bat

--- OpenWebUI.bat
+++ OpenWebUI.bat
@@ -1,6 +1,6 @@
 @echo off
 rem Activate the virtual environment
-call C:\Windows\System32\ollama\Scripts\activate.bat
+call "C:\ProgramData\miniconda3\Scripts\activate.bat" OLLAMA
 
 rem Start the open-webui server
 open-webui serve
