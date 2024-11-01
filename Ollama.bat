10ec67f66108ce8f0e8ac4b2c1b08575c1ff7863
Md-Siam-Mia-Code <mdsiammia.main@gmail.com>
2024-11-01 15:32:04 +0600

    Update Ollama.bat

--- Ollama.bat
+++ Ollama.bat
@@ -1,9 +1,9 @@
 @echo off
 rem Activate the virtual environment
-call C:\Windows\System32\ollama\Scripts\activate.bat
+call "C:\ProgramData\miniconda3\Scripts\activate.bat" GFPGAN
 
 rem Change directory to llama-cpp
-cd llama-cpp
+cd /D path/to/your/Ollama-On-Intel-ARC-GPU
 
 rem Initialize Ollama
 call init-ollama.bat
@@ -16,6 +16,7 @@ set OLLAMA_NUM_GPU=999
 set no_proxy=localhost,127.0.0.1
 set ZES_ENABLE_SYSMAN=1
 set SYCL_CACHE_PERSISTENT=1
+set OLLAMA_KEEP_ALIVE=-1
 
 rem Start the Ollama server
 ollama serve
